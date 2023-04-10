b86c71ede49cd9df28cc90f0fa361793d7c64db2
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-10 18:18:26 +0300

    more refactoring

--- firstHttpApp/handlers.go
+++ firstHttpApp/handlers.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
-
-func renderTemplate(w http.ResponseWriter, templ string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + templ)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing templates:", err)
-		return
-	}
-}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.html")
