b52853147aa2a77663f9f6ee7f63625417ac8328
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-10 15:37:04 +0300

    go is awsome

--- intro/main.go
+++ intro/main.go
@@ -1,26 +1,10 @@
 package main
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+import (
+	"log"
+)
 
 func main() {
-
-	dog := Dog{
-		Name:  "Peanut",
-		Breed: "Beagle",
-	}
+	log.Println("hello ")
 
 }
