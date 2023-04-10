5852a865368893e363e5d0bb476da18832fbb2b3
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-10 17:14:26 +0300

    playing arround with http

--- firstHttpApp/main.go
+++ firstHttpApp/main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page!")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, _ = fmt.Fprintf(w, "This is about page and 2+2 = %d", sum)
+}
+
+func AddValues(x, y int) int {
+	sum := x + y
+	return sum
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := DivideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, "%f divided by %f is equal to %f ", 100.0, 10.0, f)
+
+}
+
+func DivideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World from go")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("number of bytes written %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
